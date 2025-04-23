3040bb935ab1691dfb35f6e6125e41eb79cd9b85
Leon Froschauer <qx53cbuh@mail.leonfroschauer.de>
2025-04-23 18:20:41 +0200

    Moved tag handling classes

--- files.py
+++ files.py
@@ -1,5 +1,5 @@
 from cards import BasicCard
-from helpers.tag_handling import handle_tags, merge_tags
+
 
 class MarkdownCardFile:
     card_list = []
@@ -21,7 +21,7 @@ class MarkdownCardFile:
 
         def add_card_if_not_empty():
             if latest_question:
-                self.card_list.append(BasicCard(latest_question, latest_answer, merge_tags(tags)))
+                self.card_list.append(BasicCard(latest_question, latest_answer, self.merge_tags(tags)))
 
         for line in input_lines:
             # Case 1: Line starts with a tag (Heading in a markdown file) -> Update Tags and reset card
@@ -29,7 +29,7 @@ class MarkdownCardFile:
                 add_card_if_not_empty()
                 latest_question = ""
                 latest_answer = ""
-                tags = handle_tags(line, tags)
+                tags = self.handle_tags(line, tags)
 
             # Case 2: Line starts with a "---" -> Reset card and process the previous card
             elif line.startswith("---"):
@@ -54,3 +54,47 @@ class MarkdownCardFile:
 
         # Add the last card to the deck, since it's not covered by the for-loop
         add_card_if_not_empty()
+
+    @staticmethod
+    def handle_tags(new_heading: str, tags: list) -> list:
+        """
+        Given a new heading and a list of tags, this function updates the list of tags based on the structure of the new
+        heading.
+        :param new_heading: line of the new heading
+        :param tags: existing tags
+        :return: updated list of tags
+        """
+
+        # Remove the leading # with its whitespace and any other # characters
+        stripped_heading = new_heading.replace("# ", "").replace("#", "")
+
+        # Tag list consists of base tag and file name and is extended by each new heading
+        # If you subtract 1 from the length of tag it should match if the new heading if it is a subheading
+        if new_heading.count("#") == len(tags) - 1:
+            tags.append(stripped_heading)
+        elif new_heading.count("#") < len(tags) - 1:
+            while new_heading.count("#") < len(tags) - 1:
+                tags.pop()
+            tags.append(stripped_heading)
+        else:
+            print("Error: Wrong heading structure (Tag could not be created)")
+
+        return tags
+
+    @staticmethod
+    def merge_tags(tags: list) -> str:
+        """
+        Merge tags into a single string and apply tag formatting (replace spaces with underscores, have leading zeros
+        (09 instead of 9) add "::" between tags).
+        :param tags: list of tags
+        :return: merged tags
+        """
+
+        merged_tag = ""
+        for tag in tags:
+            if tag != "":
+                if tag[0].isdigit() and tag[0] != "0" and not tag[1].isdigit():
+                    tag = "0" + tag
+
+                merged_tag += tag.replace(". ", "_").replace(" ", "_") + "::"
+        return merged_tag[:-2] if tags else ""
