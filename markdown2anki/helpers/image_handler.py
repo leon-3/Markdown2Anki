5966db2837d6d0850233427f46f818c88200379f
Leon Froschauer <qx53cbuh@mail.leonfroschauer.de>
2025-04-25 19:14:39 +0200

    Added docstring

--- markdown2anki/helpers/image_handler.py
+++ markdown2anki/helpers/image_handler.py
@@ -11,17 +11,19 @@ class ImageHandler(Processor):
 
     def __init__(self):
         super().__init__(self.replace_md_image_with_html)
-        self.media_files = []
+
+        self.media_files = [] # Store all media files that will be added to the exported anki package
         self.tags_mapped_to_images = defaultdict(list)  # key: tag, value: list of images
-        self.image_occlusion_index = 0
-        self.used_images_mapped_to_unique_name = dict()
+        self.image_occlusion_count = 0
+        self.used_images_mapped_to_unique_name = dict() # key: original image name, value: unique name
         self.unique_image_name = ''.join(random.choices(string.ascii_uppercase + string.digits, k=8)) + "_image_"
 
         if os.path.exists("output"):
             for file in os.listdir("output"):
                 os.remove(f"output/{file}")
             os.rmdir("output")
-        print("Output folder deleted (Cause: new ImageHandler instance)")
+        print("Output folder deleted (Reason: new ImageHandler instance)")
+
 
     def replace_md_image_with_html(self, text: str) -> str:
         """
@@ -42,29 +44,37 @@ class ImageHandler(Processor):
         return text
 
     def process_image_occlusion(self, cloze_text: str, tag: str) -> None:
+        """
+        Process image occlusion by saving the images to the output folder and mapping them to the tags, since image
+        occlusion have to be handled by the user manually there is no need to add them to the package.
+        :param cloze_text: The text where the exact file name of the image is
+        :param tag: The tag that should be added to the image
+        :return: None
+        """
+
         images = re.findall(r"!\[\[.+\]\]", cloze_text)
         if not images:
             print("Error: No image found for image occlusion (tag: " + tag + ")")
             return
+
         for image in images:
-            # 1. Save image to output folder
             if not os.path.exists("output"):
                 os.makedirs("output")
+                print("Output folder created (Reason: new ImageHandler instance)")
 
             # get file extension of image
             extension = "." + image[3:-2].split(".")[-1]
             if image[3:-2] not in self.used_images_mapped_to_unique_name:
                 self.used_images_mapped_to_unique_name[image[3:-2]] = self.unique_image_name + str(
-                    self.image_occlusion_index) + extension
+                    self.image_occlusion_count) + extension
 
                 with open("input/" + image[3:-2], "rb") as f:
-                    with open("output/" + self.unique_image_name + str(self.image_occlusion_index) + extension,
+                    with open("output/" + self.unique_image_name + str(self.image_occlusion_count) + extension,
                               "wb") as f1:
                         f1.write(f.read())
 
-                # 2. Save message for further instructions
                 self.tags_mapped_to_images[tag].append(
-                    self.unique_image_name + str(self.image_occlusion_index) + extension)
-                self.image_occlusion_index += 1
+                    self.unique_image_name + str(self.image_occlusion_count) + extension)
+                self.image_occlusion_count += 1
             else:
                 self.tags_mapped_to_images[tag].append(self.used_images_mapped_to_unique_name[image[3:-2]])
