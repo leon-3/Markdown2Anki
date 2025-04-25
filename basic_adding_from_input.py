b681caee3f94652d9858a179d3900a786d7c298a
Leon Froschauer <qx53cbuh@mail.leonfroschauer.de>
2025-04-25 10:06:06 +0200

    Added basic adder

---
+++ basic_adding_from_input.py
@@ -0,0 +1,20 @@
+import os
+
+from markdown2anki import file_to_preprocessed_cards, create_cards, create_package
+from markdown2anki import ImageHandler
+
+if __name__ == "__main__":
+    image_handler = ImageHandler()
+    base_tag = "University"
+    note_list = []
+
+    for file in os.listdir("input"):
+        if file.endswith(".md"):
+            print(f"Processing file '{file}'")
+            with open(f"input/{file}", encoding="utf-8") as f:
+                basic_cards = file_to_preprocessed_cards(f.read().split("\n"), file, base_tag)
+                note_list += create_cards(basic_cards, image_handler)
+        else:
+            print(f"Skipped file '{file}' (not a markdown file)")
+
+    create_package(note_list, image_handler, "output")
